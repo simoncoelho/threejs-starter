058ad45b36d7380c6e12178c5edc6e3f409995af
Simon Coelho <spcoelho@ucdavis.edu>
2022-10-11 17:05:37 -0700

    Add plane into the scene

--- main.js
+++ main.js
@@ -4,21 +4,22 @@ const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
-console.log(scene);
-console.log(cam);
-console.log(renderer);
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
 const box = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 0.1, linecap: 'round', linejoin: 'round' });
+const boxMesh = new THREE.Mesh(box, material);
 
-const mesh = new THREE.Mesh(box, material);
-console.log(mesh);
+const plane = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMat = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 0.1, linecap: 'round', linejoin: 'round' });
+const planeMesh = new THREE.Mesh(plane, planeMat);
 
-scene.add(mesh);
+console.log()
+
+scene.add(planeMesh);
+scene.add(boxMesh);
 
 cam.position.z = 5;
 
@@ -26,9 +27,11 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, cam);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
+    boxMesh.rotation.x += 0.01;
+    boxMesh.rotation.y += 0.01;
 
+    planeMesh.rotation.x += 0.01;
+    planeMesh.rotation.y += 0.01;
 }
 
 animate();
