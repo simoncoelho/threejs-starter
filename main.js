9cb2296227301c6050a569dd2c8e442566fc1b3b
Simon Coelho <spcoelho@ucdavis.edu>
2022-10-11 17:33:31 -0700

    Add noise to the plane and create phong material

--- main.js
+++ main.js
@@ -9,13 +9,27 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const plane = new THREE.PlaneGeometry(5, 5, 10, 10);
-const planeMat = new THREE.MeshPhongMaterial({ color: 0xff0000, side: THREE.DoubleSide });
+const planeMat = new THREE.MeshPhongMaterial({ 
+    color: 0xffffff, 
+    side: THREE.DoubleSide, 
+    flatShading: true, 
+});
 const planeMesh = new THREE.Mesh(plane, planeMat);
 
 const light = new THREE.DirectionalLight(0xffffff, 1, 100);
 light.position.set(0, 0, 1);
 
-console.log()
+console.log(planeMesh.geometry.attributes.position.array);
+
+const {array} =  planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i+=3 ) {
+    const x = array[i];
+    const y = array[i+1];
+    const z = array[i+2];
+
+    array[i + 2] = z + Math.random();
+
+}
 
 scene.add(light);
 scene.add(planeMesh);
@@ -26,7 +40,7 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, cam);
     
-    planeMesh.rotation.x += 0.01;
+    //planeMesh.rotation.x += 0.01;
     //planeMesh.rotation.y += 0.01;
 }
 
