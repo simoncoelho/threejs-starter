4b2382a1b3809ec49aad7e3705ef2b975fc76613
Simon Coelho <spcoelho@ucdavis.edu>
2022-10-13 04:51:51 -0700

    Change dependency

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from './three';
 
 const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
